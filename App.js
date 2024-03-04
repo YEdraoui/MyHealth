96e1bc05141d878a147d5c3deb5e7a2a8295dddb
YEdraoui <98531970+YEdraoui@users.noreply.github.com>
2024-03-04 10:56:10 +0100

    App.js

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Choose what user you are!</Text>
+      <Button title='Patient'/>
+
+      <Button title='Pharmacy'/>
+
+      <Button title='Doctor'/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
